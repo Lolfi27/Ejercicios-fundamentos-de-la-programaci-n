aed751f31045a770a605da267e1bb102ff9a626d
Lolfi_27 <dial.he.gtz@gmail.com>
2025-09-04 20:12:58 -0600

    corregido

--- ejercicios inputs.py
+++ ejercicios inputs.py
@@ -27,11 +27,11 @@ nume1 = int(input())
 nume2 = int(input())
 nume3 = int(input())
 suma2 = (nume1 + nume2 + nume3)/3
-print = suma2
+print(suma2)
 
 #Ejercicio 7
 edad2 = int(input("En que año naciste?"))
-print = 2025 - edad2
+print(2025 - edad2)
 
 #INPUTS 8
 mascota=str(input("COMO SE LLAMA TU MASCOTA: "))
@@ -48,4 +48,4 @@ filas = 5
 for i in range(1, filas + 1):
     espacios = " " * (filas - i)
     asteriscos = "*" * (2 * i - 1)
-    print(espacios + asteriscos)
+    print(espacios+asteriscos)
