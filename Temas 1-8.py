023c8fcc6f36c46ecfbb03d111e9c34e90490d76
Lolfi_27 <dial.he.gtz@gmail.com>
2025-09-04 19:05:39 -0600

    ejercicios

--- Temas 1-8.py
+++ Temas 1-8.py
@@ -6,6 +6,28 @@ print(nombre)
 edad=int(input("CUAL ES TU EDAD: "))
 print(edad+5)
 
+#ejercicio 3
+ciu_naci = input("Donde naciste?")
+print("Naciste en " + ciu_naci)
+
+#Ejercicio 4
+print("Dame  2 numeros")
+num1 = int(input())
+num2 = int(input())
+suma = num1 + num2
+print(suma)
+
+#Ejercicio 5
+numero = int(input("Dame un numero"))
+print(numero*2)
+
+#Ejercicio 6
+print("dame 3 numeros:")
+nume1 = int(input())
+nume2 = int(input())
+nume3 = int(input())
+suma2 = (nume1 + nume2 + nume3)/3
+print = suma2
 #INPUTS 8
 mascota=str(input("COMO SE LLAMA TU MASCOTA: "))
 print(mascota.upper())
