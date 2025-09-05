b03b49e02edbf51a97113a89a53bfef120f154ac
DavidDuranRubio <AL07165261@tecmilenio.mx>
2025-09-04 20:05:01 -0600

    TRIANGULO

--- ejercicios inputs.py
+++ ejercicios inputs.py
@@ -43,4 +43,9 @@ if cali > 70:
     print("Es mayor a 70")
 else:
     print("Es menor a 70")
-
+#TRIANGULO
+filas = 5
+for i in range(1, filas + 1):
+    espacios = " " * (filas - i)
+    asteriscos = "*" * (2 * i - 1)
+    print(espacios + asteriscos)
