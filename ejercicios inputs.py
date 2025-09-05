bb4418191e093c3c335f27ce187ae0709486c5ec
Lolfi_27 <dial.he.gtz@gmail.com>
2025-09-04 19:15:43 -0600

    inputs

--- ejercicios inputs.py
+++ ejercicios inputs.py
@@ -7,7 +7,7 @@ edad=int(input("CUAL ES TU EDAD: "))
 print(edad+5)
 
 #ejercicio 3
-ciu_naci = input("Donde naciste?")
+ciu_naci = input("Donde naciste? ")
 print("Naciste en " + ciu_naci)
 
 #Ejercicio 4
@@ -28,6 +28,19 @@ nume2 = int(input())
 nume3 = int(input())
 suma2 = (nume1 + nume2 + nume3)/3
 print = suma2
+
+#Ejercicio 7
+edad2 = int(input("En que año naciste?"))
+print = 2025 - edad2
+
 #INPUTS 8
 mascota=str(input("COMO SE LLAMA TU MASCOTA: "))
 print(mascota.upper())
+
+#inputs 9 
+cali = int(input("Dame un numero del \"0\" al \"100\""))
+if cali > 70:
+    print("Es mayor a 70")
+else:
+    print("Es menor a 70")
+
